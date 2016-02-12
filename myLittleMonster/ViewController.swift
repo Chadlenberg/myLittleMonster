414b3ccfd76a31cc71482c7ba3dc16a56f81f6f4
Chad Wollenberg <chad.wollenberg@me.com>
2016-02-12 11:40:57 -0600

    Initial Commit

---
+++ myLittleMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myLittleMonster
+//
+//  Created by Chad Wollenberg on 2/12/16.
+//  Copyright © 2016 Chad Wollenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
